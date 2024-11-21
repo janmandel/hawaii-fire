4461f5d2942f09b6f2728d33012b7b28f5b666a1
Spears <t-spears@hotmail.com>
2024-11-21 16:18:06 -0700

    updates to ml_sample and cleaning directory

--- ml_sample.sh
+++ ml_sample.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-PYTHONUNBUFFERED=1 python ml_sample_generator.py > ml_sample.log 2>&1
+# A simple single-core job template for running a Python script
+#SBATCH --job-name=ml_sample_job          # Job name
+#SBATCH --partition=math-alderaan         # Partition name (adjust if necessary)
+#SBATCH --time=96:00:00                    # Maximum runtime (hh:mm:ss)
+#SBATCH --ntasks=1                        # Number of tasks (single core)
+#SBATCH --output=logs/ml_sample_%j.log         # Log output file (%j will include the job ID)
+#SBATCH --error=logs/ml_sample_%j.err          # Error output file (%j will include the job ID)
+
+PYTHONUNBUFFERED=1 python ml_sample_generator.py > logs/ml_sample.log 2>&1  
 
