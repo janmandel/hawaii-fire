21253a4315b7ad3e33536000c06a1281ea2ffe79
Spears <t-spears@hotmail.com>
2024-11-25 18:10:32 -0700

    replacing pyproj with gdal

--- conda_env.sh
+++ conda_env.sh
@@ -1,1 +1,1 @@
-conda create -n pyproj_env python=3.12 netCDF4 pyproj pandas matplotlib rasterio dbfread  pygrib -c conda-forge
+conda create -n gdal_env python=3.11 netCDF4 osgeo pandas matplotlib rasterio dbfread  pygrib -c conda-forge
