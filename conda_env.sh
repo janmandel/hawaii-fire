5f59c41530c91cb20285cf0c76eda34432926ef9
Jan Mandel <jan.mandel@gmail.com>
2024-11-25 17:53:46 -0700

    modified:   conda_env.sh

--- conda_env.sh
+++ conda_env.sh
@@ -1,1 +1,1 @@
-conda create -n pyproj_env python=3.12 netCF4 pyproj matplotlib rasterio datetime dbfread  pygrib -c conda-forge
+conda create -n pyproj_env python=3.12 netCDF4 pyproj matplotlib rasterio datetime dbfread  pygrib -c conda-forge
